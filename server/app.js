9d3878c3cf2754158bd3163297e46346885bd3aa
Gvinjilia <gvinjilian2@gmail.com>
2025-10-19 17:20:07 +0400

    commit

--- server/app.js
+++ server/app.js
@@ -22,8 +22,8 @@ app.use(cors({
 }));
 app.use(express.static(path.join(__dirname, 'dist')))
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.use(cookieParser());
